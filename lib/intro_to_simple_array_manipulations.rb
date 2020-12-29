8d06c15df214744d1bb05f1cd9c4be5a6412ff01
Robert Obi <robobi97@googlemail.com>
2020-12-29 02:42:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+require './lib/intro_to_simple_array_manipulations.rb'
+
+def using_concat(a,b)
+  my_favourite_things = ["raindrops on roses","whiskers on kittens"]
+  b = ["sports cars","flatiron school"]
+  all_my_favs = a.concat(b)
+end
+
+def using_insert(list_of_programming_languages,another_language)
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language = ["Python"]
+newarray = another_language.insert(4, another_language)
+end
